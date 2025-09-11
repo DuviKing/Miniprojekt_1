94ff6c287846e33631b19e8c4a6793d53a897d2f
DuviKing <davideinersen@outlook.dk>
2025-09-11 23:34:54 +0200

    Implement Map Square

---
+++ Miniprojekt 1/Assets/GridManager.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class GridManager : MonoBehaviour
+{
+    [SerializeField] private int _width, _height;
+    [SerializeField] private Tile tilePrefab;
+    [SerializeField] private Transform mainCamera;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        GenerateGrid();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void GenerateGrid()
+    {
+        for (int x = 0; x < _width; x++)
+        {
+            for (int y = 0; y < _height; y++)
+            {
+                var spawnedTile = Instantiate(tilePrefab, new Vector3(x, y), Quaternion.identity);
+                spawnedTile.name = $"Tile {x} {y}";
+
+                var isOffset = (x % 2 == 0 && y % 2 != 0) || (x % 2 != 0 && y % 2 == 0);
+                spawnedTile.Init(isOffset);
+            }
+        }
+
+
+        mainCamera.transform.position = new Vector3((float)_width / 2 - 0.5f, (float)_height / 2 - 0.5f, -10);
+    }
+}
