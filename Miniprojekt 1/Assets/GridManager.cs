deea628f9126653e8213066012618c262cab59d9
DuviKing <davideinersen@outlook.dk>
2025-09-13 15:10:21 +0200

    HexMap Generation Working
    Nu virker map generations scriptet (hvis størrelsen ikke bliver for stor)

--- Miniprojekt 1/Assets/GridManager.cs
+++ Miniprojekt 1/Assets/GridManager.cs
@@ -27,19 +27,34 @@ public class GridManager : MonoBehaviour
         {
             for (int y = -gridSizeRadius; y <= gridSizeRadius; y += 1) //Loop'er gennem alle heltalskoordinater
             {
+                //Definerer tile afstande
+                float xTilePos = y % 2 == 0 ? x : x + 0.5f;
+                float yTilePos = y % 2 == 0 ? y - 0.25f * y : y - 0.25f * y;
+                float distCentPos = MathF.Sqrt(xTilePos * xTilePos + yTilePos * yTilePos);
+                float distCent = MathF.Sqrt(x * x + y * y);
+                float distPos = MathF.Abs(xTilePos) + MathF.Abs(yTilePos);
+
+                //Tjek om tile positionen er inde i hexagonen med radius på gridSizeRadius
+                if (distCent < gridSizeRadius &&
+                distCentPos < gridSizeRadius &&
+                (MathF.Abs(xTilePos) <= (float)gridSizeRadius / 2 || distPos <= gridSizeRadius))
+                {
                     //Spawner et tile på alle heltals x- og y-koordinater
-                var spawnedTile = Instantiate(tilePrefab, GetGridPosition(x, y), Quaternion.identity);
+                    var spawnedTile = Instantiate(tilePrefab, GetGridPosition(x, y), Quaternion.identity);
                     spawnedTile.name = $"Tile {x} {y}";
 
                     //Farver hver andet tile
                     var isOffset = (x % 2 == 0 && y % 2 != 0) || (x % 2 != 0 && y % 2 == 0);
                     spawnedTile.Init(isOffset);
+                }
             }
         }
     }
 
     public Vector3 GetGridPosition(int x, int y) //Beregner hexagon positionen ud fra et firkantet koordinatsystem
     {
-        return new Vector3(y % 2 == 0 ? x : x + 0.5f, y % 2 == 0 ? y - 0.25f * y : y - 0.25f * y, 0);
+        float xPos = y % 2 == 0 ? x : x + 0.5f;
+        float yPos = y % 2 == 0 ? y - 0.25f * y : y - 0.25f * y;
+        return new Vector3(xPos, yPos, 0);
     }
 }
