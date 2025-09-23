7d62b52aca1efb4f3927c1f70f237ecdd53c4e71
DuviKing <davideinersen@outlook.dk>
2025-09-23 12:48:09 +0200

    Update LydMixserManeger.cs

--- Miniprojekt 1/Assets/LydMixserManeger.cs
+++ Miniprojekt 1/Assets/LydMixserManeger.cs
@@ -12,14 +12,14 @@ public class LydMixserManeger : MonoBehaviour
 
     public void setmasterVolum(float volum)
     {
-    lydMixser.SetFloat("MasterVolume", Mathf.Log10(volum) * 20);
+        lydMixser.SetFloat("MasterVolume", Mathf.Log10(volum) * 20);
     }
     public void setmusikkVolum(float volum)
     {
-    lydMixser.SetFloat("music volume", Mathf.Log10(volum) * 20);
+        lydMixser.SetFloat("music volume", Mathf.Log10(volum) * 20);
     }
     public void seteffektVolum(float volum)
     {
-    lydMixser.SetFloat("LydeffektVolume", Mathf.Log10(volum) * 20);
+        lydMixser.SetFloat("LydeffektVolume", Mathf.Log10(volum) * 20);
     }
 }
